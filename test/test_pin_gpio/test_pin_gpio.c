edcdb4fb145ea58abc246a4492fd1253c7f87415
Balthazar Martin <balthazarm8@gmail.com>
2022-06-23 22:48:35 -0300

    refactory de lo recien echo

--- test/test_pin_gpio/test_pin_gpio.c
+++ test/test_pin_gpio/test_pin_gpio.c
@@ -16,32 +16,25 @@ static  Pin miPin; //utilizo de la libreria pin_gpio.h para definir pines. debo 
 void setUp(void) //lo que hara al iniciar una prueba
 {
     miPuerto = (GPIO_TypeDef){.CRH=0x44444444, . CRL=0x44444444};   //miPuerto es una estructura. lo que se hace aqui es copiar la estructura de la igualdad de la derecha hacia la izquierda | aqui ya se creo el puerto falso
-
+    Pin_init(&miPin, &miPuerto, MI_PIN); //lo escribo aqui por que se repite en todas las pruebas
 }
 void tearDown(void){}
 
 //creo prueba
 void pin_debePermitirLeerEstado_0(void) // es imporante tomar en cuenta que sea static o global. que sea static me hara tomar en cuenta si la utilizo o no (por que es local). no me avisara si llegase a ser global
 {
-    Pin miPin;
-    Pin_init(&miPin, &miPuerto, MI_PIN); //se necesitara de esta funcion en el header que crearemos para tomar en cuenta esos argumentos que necesitamos
-            //^ toma direcciones
     const bool estado= Pin_lee(&miPin); //para definir bool (logica de bool) necesito la libreria stdbool | ademas se usara de una funcion Pin_lee que devolvera un booleano | const permite que sea de lectura nada mas
     TEST_ASSERT_FALSE(estado); //se agrega el FALSE para que espere una respuesta falsa
     
 }
 void pin_debePermitirLeerEstado_1(void)
 {
-    Pin miPin;
-    Pin_init(&miPin, &miPuerto, MI_PIN);
     miPuerto.IDR = 1 << MI_PIN;
     const bool estado= Pin_lee(&miPin); 
     TEST_ASSERT_TRUE(estado);
 }
 void pin_debePermitirPonerSalidaEnCero(void) 
 {
-    Pin miPin; //dicha variable se la define varias veces => esta sera un posible refactory
-    Pin_init(&miPin, &miPuerto, MI_PIN);
     Pin_escribe(&miPin, 0);
     const uint32_t reset = miPuerto.BRR | (miPuerto.BSRR >> 16); //creo resultado RESET
 //  ^ constante                         ^ or  
@@ -53,14 +46,10 @@ void pin_debePermitirPonerSalidaEnCero(void)
 }
 void pin_debePermitirPonerSalidaEnUno(void) 
 {
-    Pin miPin; 
-    Pin_init(&miPin, &miPuerto, MI_PIN);
     Pin_escribe(&miPin, 1);
     const uint32_t reset = miPuerto.BRR | (miPuerto.BSRR >> 16); //creo resultado RESET
-//  ^ constante                         ^ or  
     const uint32_t set = miPuerto.BSRR & ((1UL << 16)- 1UL) //para volver mas rigurosa la funcion
     enum{ RESET_ESPERADO = 0, SET_ESPERADO (1UL << MI_PIN)}; //creo resultado RESET_ESPERADO
-//                           ^Unsigned Long
     TEST_ASSERT_EQUAL_HEX32(reset, RESET_ESPERADO);
     TEST_ASSERT_EQUAL_HEX32(set, SET_ESPERADO);
 }
